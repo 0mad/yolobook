b6c997a7b942057bde98bd5a9a348dd339412772
yunheur <yunheur@gmail.com>
2019-06-08 15:23:13 +0900

    #13 사용자 정보 페이지 - 친구 - 사용자 정보 페이지 조립

--- frontend/components/Friend/index.ts
+++ frontend/components/Friend/index.ts
@@ -1,1 +1,1 @@
-export { default } from './Friend';
+export { default } from './FriendList';
---
+++ frontend/containers/FriendContainer.tsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import FriendList from '../components/Friend';
+
+class FriendContainer extends Component<any> {
+  public render() {
+    const friendList = [];
+    for (let i = 0; i < 30; i++) {
+      friendList.push({
+        img: 'http://placekitten.com/1000/1000',
+        name: '문태민',
+        cnt: '1000',
+      });
+    }
+
+    return <FriendList friendList={friendList} />;
+  }
+}
+
+export default FriendContainer;
