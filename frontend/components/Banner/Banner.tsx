86306e86e431b2b6584047b8310033892d3a03d3
yunheur <yunheur@gmail.com>
2019-07-06 12:42:32 +0900

    #136 이미지 업로드시 미리보기 컴포넌트가 나타나도록 처리 - 포스트 작성 컴포넌트에 이미지 업로드시 미리보기 컴포넌트가 나타나도록 처리

--- frontend/components/Banner/Banner.tsx
+++ frontend/components/Banner/Banner.tsx
@@ -124,7 +124,6 @@ const Banner = ({
         )}
       </>
     )
-    return false;
   }
 
   return (
--- frontend/components/Editor/Editor.stories.tsx
+++ frontend/components/Editor/Editor.stories.tsx
@@ -5,11 +5,21 @@ storiesOf('Editor', module).add('포스트 작성', () => {
   const profile = {
     id: 98,
     username: 'yolo',
-    thumbnail: 'http://placekitten.com/40/40'
+    thumbnail: 'http://placekitten.com/40/40',
+  };
+
+  const imgUrls: Array<object> = [];
+  for (let i = 0; i < 10; i++) {
+    imgUrls.push({ url: 'http://placekitten.com/500/300' });
   }
+
   return (
-    <div style={{ padding: '20px', backgroundColor: 'gray' }}>
-      <Editor profile={profile}/>
+    <div style={{ padding: '20px' }}>
+      <h3>이미지 업로드가 안된 경우</h3>
+      <Editor profile={profile} />
+      <br />
+      <h3>이미지 업로드가 된 경우</h3>
+      <Editor profile={profile} imgUrls={imgUrls} />
     </div>
-  )
+  );
 });
